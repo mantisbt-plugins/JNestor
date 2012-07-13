e6a71faa34fe27687c1a246a9fe65b42d602546f
Raul Luna <superman_ha_muerto@yahoo.com>
2012-07-13 23:55:04 +0200

    delete of source code

--- JNestor/README.md
+++
@@ -1,4 +0,0 @@
-JNestor
-=======
-
-JNestor converts your emails in mantis issues. JNestor adds the issue number to your emails if you want and have several custom configurations
--- JNestor/src/com/supermanhamuerto/nestor/mantis/MantisProcessorAction.java
+++
@@ -1,9 +0,0 @@
-package com.supermanhamuerto.nestor.mantis;
-
-import com.supermanhamuerto.exception.JNException;
-import com.supermanhamuerto.nestor.MailMessage;
-
-public interface MantisProcessorAction
-{
-	public void doAction( long issueId, MailMessage msg, boolean exists ) throws JNException;
-}
